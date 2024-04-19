4c5f9b524e91a6ace515795128de2d4a14bdcb74
victorAsksWhy <vlsmarthen@gmail.com>
2024-04-19 07:39:15 -0400

    add spam commands=

--- bot.py
+++ bot.py
@@ -19,12 +19,21 @@ bot = commands.Bot(command_prefix='.', description=description, intents=intents)
 
 @bot.event
 async def on_ready():
-    print(f'Logged in as {bot.user} (ID: {bot.user.id})')
+    print(f'Logged in as {bot.user} with ID: {bot.user.id}')
     print('---------------')
 
 
 @bot.command()
 async def womp(ctx):
+    '''Post a womp womp womp gif'''
     await ctx.send(file=discord.File('giphy.gif'))
+
+@bot.command()
+async def spam(ctx, message, times):
+    '''Spam a message'''
+    times = int(times)
+    for x in range(times):
+        await ctx.send(message)
+
 
 bot.run('MTIzMDE1MTk2Njk3MDM0NzY0MQ.Gp97DL.wPtZf6xxcpIgmg4_jEaCcCfZ0WZ4tDnJUqrtH8')
